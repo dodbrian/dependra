8db8d7f1f362dd300bc8d18f54d9c49072304f65
Denis Zimin <roydronov@gmail.com>
2020-07-12 23:38:14 +0200

    get paths of referenced projects

--- src/Dependra/Services/SolutionLoader.cs
+++ src/Dependra/Services/SolutionLoader.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using Dependra.Domain;
 
 namespace Dependra.Services
@@ -11,10 +12,45 @@ namespace Dependra.Services
             var projectPaths = Directory.GetFiles(pathToSolution, "*.csproj", SearchOption.AllDirectories);
             var solution = new Solution();
 
-            var projects = projectPaths.Select(path => new Project(path));
+            var projects = projectPaths
+                .Select(path => new Project(path))
+                .ToList();
+
             solution.AddProjectsRange(projects);
 
+            foreach (var project in projects)
+            {
+                var projectXml = XDocument.Load(project.FullPath);
+                var referencedProjectPaths = projectXml.Root?
+                    .Descendants("ProjectReference")
+                    .Select(element => element.Attribute("Include")?.Value)
+                    .Where(path => path != null)
+                    .Select(path => GetAbsolutePath(project, path))
+                    .ToList();
+
+                // project.AddReferencedProject();
+            }
+
             return solution;
+        }
+
+        private static string GetAbsolutePath(Project project, string path)
+        {
+            var directoryName = Path.GetDirectoryName(project.FullPath);
+            var normalizedPath = NormalizePath(path);
+            var combinedPath = Path.Combine(directoryName ?? string.Empty, normalizedPath);
+            var absolutePath = Path.GetFullPath(combinedPath);
+
+            return absolutePath;
+        }
+
+        private static string NormalizePath(string path)
+        {
+            var normalizedPath = path
+                .Replace('\\', Path.DirectorySeparatorChar)
+                .Replace('/', Path.DirectorySeparatorChar);
+
+            return normalizedPath;
         }
     }
 }
