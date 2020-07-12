81d5a2b93f8c7d88d6827f5d109fd399d01a0cd4
Denis Zimin <roydronov@gmail.com>
2020-07-12 21:49:05 +0200

    refactor project addition

--- src/Dependra/Domain/Solution.cs
+++ src/Dependra/Domain/Solution.cs
@@ -7,11 +7,19 @@ namespace Dependra.Domain
         private readonly Dictionary<string, Project> _projects = new Dictionary<string, Project>();
         public int NumberOfProjects => _projects.Count;
 
-        public void AddProject(string pathToProject)
+        public void AddProject(Project project)
         {
-            if (_projects.TryGetValue(pathToProject, out _)) return;
+            if (_projects.TryGetValue(project.FullPath, out _)) return;
 
-            _projects[pathToProject] = new Project(pathToProject);
+            _projects[project.FullPath] = project;
+        }
+
+        public void AddProjectsRange(IEnumerable<Project> projects)
+        {
+            foreach (var project in projects)
+            {
+                AddProject(project);
+            }
         }
     }
 }
--- src/Dependra/Services/SolutionLoader.cs
+++ src/Dependra/Services/SolutionLoader.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Linq;
 using Dependra.Domain;
 
 namespace Dependra.Services
@@ -10,10 +11,8 @@ namespace Dependra.Services
             var projectPaths = Directory.GetFiles(pathToSolution, "*.csproj", SearchOption.AllDirectories);
             var solution = new Solution();
 
-            foreach (var pathToProject in projectPaths)
-            {
-                solution.AddProject(pathToProject);
-            }
+            var projects = projectPaths.Select(path => new Project(path));
+            solution.AddProjectsRange(projects);
 
             return solution;
         }
