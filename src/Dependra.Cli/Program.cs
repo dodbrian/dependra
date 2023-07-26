b96b9deb3d3a6c6cf163989438cc8414a46fc857
Denis Zimin <roydronov@gmail.com>
2023-07-27 00:42:11 +0200

    upgrade to net7

--- src/Dependra.Cli/Program.cs
+++ src/Dependra.Cli/Program.cs
@@ -4,45 +4,43 @@ using System.Linq;
 using System.Text.Json;
 using Dependra.Services;
 
-namespace Dependra.Cli
+namespace Dependra.Cli;
+
+internal static class Program
 {
-    internal static class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-            string pathToSolution;
+        string pathToSolution;
 
-            if (!args.Any() || string.IsNullOrWhiteSpace(args[0]))
-            {
-                pathToSolution = Directory.GetCurrentDirectory();
-            }
-            else
-            {
-                pathToSolution = args[0];
-            }
+        if (!args.Any() || string.IsNullOrWhiteSpace(args[0]))
+            pathToSolution = Directory.GetCurrentDirectory();
+        else
+            pathToSolution = args[0];
 
-            var fileService = new FileService();
-            var solutionLoader = new SolutionLoader(fileService, new ProjectLoader(fileService));
-            var solution = solutionLoader.LoadFromPath(pathToSolution);
+        var fileService = new FileService();
+        var solutionLoader = new SolutionLoader(fileService, new ProjectLoader(fileService));
+        var solution = solutionLoader.LoadFromPath(pathToSolution);
 
-            Console.WriteLine($"Solution folder: {solution.Path}");
-            Console.WriteLine($"Number of projects in solution: {solution.NumberOfProjects}");
-            Console.WriteLine($"Number of packages in solution: {solution.NumberOfPackages}");
+        Console.WriteLine($"Solution folder: {solution.Path}");
+        Console.WriteLine($"Number of projects in solution: {solution.NumberOfProjects}");
+        Console.WriteLine($"Number of packages in solution: {solution.NumberOfPackages}");
 
-            var projection = solution.Projects
-                .Select(project => new
+        var projection = solution.Projects
+            .Select(
+                project => new
                 {
                     project.FullPath,
                     Packages = project.ReferencedPackages.Select(package => $"{package.Name}, {package.Version}"),
                     Projects = project.ReferencedProjects.Select(refProject => refProject.FullPath)
                 });
 
-            var json = JsonSerializer.Serialize(projection, new JsonSerializerOptions
+        var json = JsonSerializer.Serialize(
+            projection,
+            new JsonSerializerOptions
             {
                 WriteIndented = true
             });
 
-            Console.WriteLine(json);
-        }
+        Console.WriteLine(json);
     }
 }
